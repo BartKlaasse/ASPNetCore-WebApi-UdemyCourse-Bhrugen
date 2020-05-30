99094d4ae83998babe2a20a8cad36502661a3dd2
Bart Klaasse <bart@bartklaasse.nl>
2020-05-30 23:45:44 +0200

    Configured Automapper

---
+++ ParkyMapper/ParkyMappings.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using ParkyAPI.Models;
+using ParkyAPI.Models.DTO;
+
+namespace ParkyAPI.ParkyMapper
+{
+    public class ParkyMappings : Profile
+    {
+        public ParkyMappings()
+        {
+            CreateMap<NationalPark, NationalParkDTO>().ReverseMap();
+        }
+    }
+}
