06424ff083456067cbe39f28256d6c96b3b183ef
Bart Klaasse <bart@bartklaasse.nl>
2020-05-31 23:03:41 +0200

    create IRepository

---
+++ ParkyMVC/Repository/IRepository/IRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyMVC.Repository.IRepository
+{
+    //FLOW: Making a generic Interface repository so we can use that for the trails and national parks repository
+    interface IRepository<T> where T : class
+    {
+        Task<T> GetAsync(string url, int Id);
+        Task<IEnumerable<T>> GetAllAsync(string url);
+        Task<bool> CreateAsync(string url, T objToCreate);
+        Task<bool> UpdateAsync(string url, T objToCreate);
+        Task<bool> DeleteAsync(string url, int Id);
+    }
+}
