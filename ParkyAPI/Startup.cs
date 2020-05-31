4d20118cc5377f9e77b3d79baf58aedd82fc9d1c
Bart Klaasse <bart@bartklaasse.nl>
2020-05-31 18:07:17 +0200

    api verisoning part 2

--- ParkyAPI/Startup.cs
+++ ParkyAPI/Startup.cs
@@ -9,15 +9,18 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using ParkyAPI.Data;
 using ParkyAPI.ParkyMapper;
 using ParkyAPI.Repository;
 using ParkyAPI.Repository.IRepository;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace ParkyAPI
 {
@@ -43,72 +46,75 @@ namespace ParkyAPI
                 options.DefaultApiVersion = new ApiVersion(1, 0);
                 options.ReportApiVersions = true;
             });
-            services.AddSwaggerGen(options =>
-            {
-                // options.SwaggerDoc("ParkyOpenApiSpecNP",
-                //     new Microsoft.OpenApi.Models.OpenApiInfo()
-                //     {
-                //         Title = "ParkyApi NationalParks",
-                //             Version = "1",
-                //             Description = "Udemy Parky Api for national parkscourse by bhrugen patel",
-                //             Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                //             {
-                //                 Email = "name@example.com",
-                //                     Name = "Bart Klaasse",
-                //                     Url = new Uri("Https://google.com")
-                //             },
-                //             License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                //             {
-                //                 Name = "Custom license",
-                //                     Url = new Uri("https://google.com")
-                //             }
-                //     });
-                // options.SwaggerDoc("ParkyOpenApiSpecTrails",
-                //     new Microsoft.OpenApi.Models.OpenApiInfo()
-                //     {
-                //         Title = "ParkyApi Trails",
-                //             Version = "1",
-                //             Description = "Udemy Parky Api for trails course by bhrugen patel",
-                //             Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                //             {
-                //                 Email = "name@example.com",
-                //                     Name = "Bart Klaasse",
-                //                     Url = new Uri("Https://google.com")
-                //             },
-                //             License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                //             {
-                //                 Name = "Custom license",
-                //                     Url = new Uri("https://google.com")
-                //             }
-                //     });                
-                options.SwaggerDoc("ParkyOpenApiSpec",
-                    new Microsoft.OpenApi.Models.OpenApiInfo()
-                    {
-                        Title = "ParkyApi",
-                            Version = "1",
-                            Description = "Udemy Parky Api for course by bhrugen patel",
-                            Contact = new Microsoft.OpenApi.Models.OpenApiContact()
-                            {
-                                Email = "name@example.com",
-                                    Name = "Bart Klaasse",
-                                    Url = new Uri("Https://google.com")
-                            },
-                            License = new Microsoft.OpenApi.Models.OpenApiLicense()
-                            {
-                                Name = "Custom license",
-                                    Url = new Uri("https://google.com")
-                            }
-                    });
-                //FLOW: Get the xml file from the root folder of the project.
-                var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
-                options.IncludeXmlComments(xmlCommentsFullPath);
-            });
+            services.AddVersionedApiExplorer(options => options.GroupNameFormat = "'v'VVV");
+            services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+            services.AddSwaggerGen();
+            // services.AddSwaggerGen(options =>
+            // {
+            //     // options.SwaggerDoc("ParkyOpenApiSpecNP",
+            //     //     new Microsoft.OpenApi.Models.OpenApiInfo()
+            //     //     {
+            //     //         Title = "ParkyApi NationalParks",
+            //     //             Version = "1",
+            //     //             Description = "Udemy Parky Api for national parkscourse by bhrugen patel",
+            //     //             Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //     //             {
+            //     //                 Email = "name@example.com",
+            //     //                     Name = "Bart Klaasse",
+            //     //                     Url = new Uri("Https://google.com")
+            //     //             },
+            //     //             License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //     //             {
+            //     //                 Name = "Custom license",
+            //     //                     Url = new Uri("https://google.com")
+            //     //             }
+            //     //     });
+            //     // options.SwaggerDoc("ParkyOpenApiSpecTrails",
+            //     //     new Microsoft.OpenApi.Models.OpenApiInfo()
+            //     //     {
+            //     //         Title = "ParkyApi Trails",
+            //     //             Version = "1",
+            //     //             Description = "Udemy Parky Api for trails course by bhrugen patel",
+            //     //             Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //     //             {
+            //     //                 Email = "name@example.com",
+            //     //                     Name = "Bart Klaasse",
+            //     //                     Url = new Uri("Https://google.com")
+            //     //             },
+            //     //             License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //     //             {
+            //     //                 Name = "Custom license",
+            //     //                     Url = new Uri("https://google.com")
+            //     //             }
+            //     //     });                
+            //     options.SwaggerDoc("ParkyOpenApiSpec",
+            //         new Microsoft.OpenApi.Models.OpenApiInfo()
+            //         {
+            //             Title = "ParkyApi",
+            //                 Version = "1",
+            //                 Description = "Udemy Parky Api for course by bhrugen patel",
+            //                 Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+            //                 {
+            //                     Email = "name@example.com",
+            //                         Name = "Bart Klaasse",
+            //                         Url = new Uri("Https://google.com")
+            //                 },
+            //                 License = new Microsoft.OpenApi.Models.OpenApiLicense()
+            //                 {
+            //                     Name = "Custom license",
+            //                         Url = new Uri("https://google.com")
+            //                 }
+            //         });
+            //     //FLOW: Get the xml file from the root folder of the project.
+            //     var xmlCommentsFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            //     var xmlCommentsFullPath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFile);
+            //     options.IncludeXmlComments(xmlCommentsFullPath);
+            // });
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IApiVersionDescriptionProvider provider)
         {
             if (env.IsDevelopment())
             {
