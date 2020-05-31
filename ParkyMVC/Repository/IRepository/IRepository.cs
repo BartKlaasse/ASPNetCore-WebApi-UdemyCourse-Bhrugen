4916d3d4a23fd795f0f396256696b2f03737347f
Bart Klaasse <bart@bartklaasse.nl>
2020-05-31 23:16:22 +0200

    finished repository

--- ParkyMVC/Repository/IRepository/IRepository.cs
+++ ParkyMVC/Repository/IRepository/IRepository.cs
@@ -11,7 +11,7 @@ namespace ParkyMVC.Repository.IRepository
         Task<T> GetAsync(string url, int Id);
         Task<IEnumerable<T>> GetAllAsync(string url);
         Task<bool> CreateAsync(string url, T objToCreate);
-        Task<bool> UpdateAsync(string url, T objToCreate);
+        Task<bool> UpdateAsync(string url, T objToUpdate);
         Task<bool> DeleteAsync(string url, int Id);
     }
 }
