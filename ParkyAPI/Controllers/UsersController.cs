64a2703baa265ed4c51d55dad944dd7c6e57537b
Bart Klaasse <bart@bartklaasse.nl>
2020-06-01 22:04:09 +0200

    Add authenticate

---
+++ ParkyAPI/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Controllers
+{
+    [Authorize]
+    [Route("api/v{version:apiVersion}/Users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+        public UsersController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User model)
+        {
+            var user = _userRepo.Authenticate(model.Username, model.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Login Details are not correct" });
+            }
+            return Ok(user);
+        }
+    }
+}
