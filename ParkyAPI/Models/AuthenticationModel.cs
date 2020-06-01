f8a49d2caf8c45eda746e32f7ed3a5c429af4448
Bart Klaasse <bart@bartklaasse.nl>
2020-06-01 22:55:26 +0200

    custom auth model

---
+++ ParkyAPI/Models/AuthenticationModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParkyAPI.Models
+{
+    public class AuthenticationModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+    }
+}
