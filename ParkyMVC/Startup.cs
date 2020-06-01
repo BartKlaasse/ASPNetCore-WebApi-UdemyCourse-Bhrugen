c4335482f33a39c3f810c101ee85cf0dbcb088d8
Bart Klaasse <bart@bartklaasse.nl>
2020-06-01 23:54:36 +0200

    setup cookie auth

--- ParkyMVC/Startup.cs
+++ ParkyMVC/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -26,6 +27,15 @@ namespace ParkyMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+                options.LoginPath = "/Home/Login";
+                options.AccessDeniedPath = "/Home/AccesDenied";
+                options.SlidingExpiration = true;
+            });
+            services.AddHttpContextAccessor();
             services.AddScoped<INationalParkRepository, NationalParkRepository>();
             services.AddScoped<ITrailRepository, TrailRepository>();
             services.AddScoped<IAccountRepository, AccountRepository>();
@@ -62,6 +72,7 @@ namespace ParkyMVC
                 .AllowAnyHeader());
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
